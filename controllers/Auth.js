e31fa01e67385c4162039c736571fb6c7be869b1
Akshat <iamakku0.0.0.1@gmail.com>
2023-06-20 23:39:06 +0530

    token created and cookie also done with login controller

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -1,6 +1,9 @@
 const bcrypt = require("bcrypt");
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 
+require("dotenv").config();
+const secretKey = process.env.JWT_secret;
 
 
 //signup route handler
@@ -32,7 +35,7 @@ exports.signup = async(req , res)=>{
         }
 
         //create user entry in db 
-           const user = await User.create({name,email,password,role});
+           const user = await User.create({name,email,password:hashedPassword,role});
            return res.status(201).json({
                success:true,
                message:"user created succesfully"
@@ -51,9 +54,78 @@ exports.signup = async(req , res)=>{
 //login route handler
 exports.login = async (req , res)=>{
     try{
+        //fetch data from the req body
+        const { email , password } = req.body;
+
+        //validation on email and password 
+        //agar email aur password m data nahi hai
+        if(!email || !password){
+            return res.status(400).json({
+                success:false,
+                message:"data is not filled please fill the data"
+            });
+        }
+
+        //check kro user exist karta hai ya nai
+        const user =  await User.findOne({email});
+        //agar email nahi match hoti toh , if not a registered user
+        if(!user){
+            return res.status(401).json({
+                success:false,
+                message:"user does not exist go and sign up first"
+            }) 
+        }
+
+        //payload for tokens
+        const payload = {
+            email: user.email,
+            id: user._id,
+            role: user.role
+        }
+
+        //verify password and generate a JWT token 
+        //to verify password we will use .compare method and pass request body password and hashed password to compare it and verify it
+        if(await bcrypt.compare(password , user.password)){
+
+            //if password matched , agar password match hojata hai toh token create kro
+            //creating token
+            let token = jwt.sign( payload , secretKey , { expiresIn:"2h"});
+
+            //jo info data base m se fetch kari hai aur user object m daldi
+            //usme token naam ki new entry daldi
+            user.token = token;
+            //aur uss user object m se password hta dia , data base m se nai htaya password jo user fetch kia hai db m se us se htaya hai kyu ki usko hum as a response send karngy toh password nai hona chaie 
+            user.password = undefined;
+
+            const options = {
+                //3 din m expire hojaega cookie 
+                expires: new Date(Date.now() + 3* 24 * 60 *60 *1000),
+                //client side p access nai kar skty cookie ko
+                httpOnly: true,
+            }
+            //creating cookie
+            res.cookie("token", token , options).status(200).json({
+                success:true,
+                token,
+                user,
+                message:"user logged in succesfully"
+            })
+        }
+        else{
+            //when password does not match
+            return res.status(403).json({
+                success:false,
+                message:"password is incorrect"
+            })
+        }
+       
 
     }
-    catch{
-
+    catch(err){
+        console.log(err)
+       return res.status(500).json({
+            success:false,
+            message:"unable to login please try again later"
+        })
     }
 }
