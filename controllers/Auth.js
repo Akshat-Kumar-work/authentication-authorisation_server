8041987612647bcd37a05eca38b8bebcf306dc13
Akshat <iamakku0.0.0.1@gmail.com>
2023-06-20 16:42:33 +0530

    sign up controller done

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -2,12 +2,58 @@ const bcrypt = require("bcrypt");
 const User = require("../models/user");
 
 
+
 //signup route handler
 exports.signup = async(req , res)=>{
     try{
-        
+        //fetch the data
+        const {name , email , password , role } = req.body;
+
+        //check if user already exist , check karo data base m ki jo email request m ayi hai vse koi email db m present hai ya nai
+        const existingUser = await User.findOne({email});
+        if(existingUser){
+            return res.status(400).json({
+                success:false,
+                message:"user already exist"
+            })
+        }
+
+        //secure password using hash function present in bcrypt library
+        let hashedPassword;
+        try{
+            hashedPassword = await bcrypt.hash(password,10);
+        }
+        catch(err){
+            console.log(err)
+            return res.status(500),json({
+                success:false,
+                message:"err in hashing password",
+            })
+        }
+
+        //create user entry in db 
+           const user = await User.create({name,email,password,role});
+           return res.status(201).json({
+               success:true,
+               message:"user created succesfully"
+            });
     }
     catch(err){
+        console.log(err)
+        return res.status(500).json({
+            success:false,
+            message:"user cannot register please try again later"
+        })
+    }
+}
+
+
+//login route handler
+exports.login = async (req , res)=>{
+    try{
+
+    }
+    catch{
 
     }
 }
--- models/user.js
+++ models/user.js
@@ -17,6 +17,7 @@ const userSchema = new mongoose.Schema({
     role:{
         type:String,
         //enum means enn teen value m se hi koi ek hoga , by defining enum hmara role bs y jo 3 value define kri hai unhe hi le skta hai
+        //role m hum bs Admin Student Visitor hi dal skty hai aur kuch dal kr req bhjengy toh fat jaega entry ni create hogi
         enum:["Admin","Student","Visitor"]
     }
 })
